9bc88e3513c58c61104e56153eafcda2b56503aa
arnoldomuller <arnoldomuller@522650dd-5330-0410-bccd-6103c8fbf572>
2007-05-21 12:43:50 +0000

    Designing OB

---
+++ trunk/src/main/java/org/ajmm/obsearch/Index.java
@@ -0,0 +1,85 @@
+/*
+    OBSearch: a distributed similarity search engine
+    This project is to similarity search what 'bit-torrent' is to downloads.
+    Copyright (C)  2007 Arnoldo Jose Muller Molina
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.   
+*/
+/** 
+		Class: Index
+		
+		An Index stores objects based on a distance function in a hopefully
+		efficient way
+    @author      Arnoldo Jose Muller Molina    
+    @version     %I%, %G%
+    @since       0.0
+*/
+public interface Index {
+    /**
+			 Searches the Index and returns Result elements (ID and distance only) 
+       that are closer to "object".
+			 The closest element is at the beginning of the list and the
+       farthest elements is at the end of the list.
+			 This condition must hold result.length == k
+
+			 @param object The object that has to be searched
+       @param k The maximum number of objects to be returned
+			 @param r the range to be used 
+			 @param result An array of "Result". A null object terminates the list. Note that if the list contains objects they will be reused saving precious time. This list will contain at most k objects.
+			 @since 0.0
+     */
+		public void searchID(OB object, byte k, DimensionHandler r, Result[] result);
+		
+		/**
+			 Searches the Index and returns OBResult (ID, OB and distance) elements
+       that are closer to "object".
+			 The closest element is at the beginning of the list and
+			 the farthest elements is at the end of the list
+			 This condition must hold result.length == k
+
+			 @param object The object that has to be searched
+       @param k The maximum number of objects to be returned
+			 @param r the range to be used 
+			 @param result An array of "OBResult". A null object terminates the list. Note that if the list contains objects they will be reused saving precious time. This list will contain at most k objects.
+			 @since 0.0
+     */
+	  public void searchOB(OB object, byte k, DimensionHandler r, OBResult[] result);
+	
+
+		/**
+			Inserts the given object into the index with the given ID      
+			If the given ID already exists, the exception IllegalIDException
+			is thrown.
+		  @param object The object to be added
+			@return 0 if the object already existed or 1 if the object was inserted
+			@throws IllegalIDException if the given ID already exists
+			@since 0.0
+		 */ 
+		 //TODO: make sure that the community is ok with storing 2,147,483,647 objects
+		 // the IDs must be provided because the ID control has to be centralized
+		public byte insert(OB object, int ID) throws IllegalIDException;
+
+		
+		/**
+    	Deletes the given object form the database
+			@param object The object to be deleted
+			@return 0 if the object was not found in the database or 1 if it
+			was deleted successfully  
+			@since 0.0
+		 */ 
+		public byte delete(OB object);
+		
+		
+}
