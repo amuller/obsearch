1ca0a45ef0f883a5196aadc12342c72d4f5acda7
arnoldomuller <arnoldomuller@522650dd-5330-0410-bccd-6103c8fbf572>
2007-05-28 10:00:07 +0000

    Ugly dependency: Berkeley db version 3, a script has to be run.

---
+++ trunk/install.sh
@@ -0,0 +1,11 @@
+
+# this script loads the latest versions of some dependencies not 
+# available in maven repositories
+# get the latest version of je
+wget http://obsearch.googlecode.com/files/je-3.2.23.tar.gz
+tar -xzf je-3.2.23.tar.gz
+
+mvn install:install-file -Dfile=./je-3.2.23/lib/je-3.2.23.jar -DgroupId=berkeleydb -DartifactId=je -Dversion=3.2.23 -Dpackaging=jar -DgeneratePom=true
+
+#cleanup
+rm -fdr je-3.2.23
