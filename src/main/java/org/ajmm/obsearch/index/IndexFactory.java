2e409043ed4ea76a9ebe5c492b4406f66419f171
arnoldomuller <arnoldomuller@522650dd-5330-0410-bccd-6103c8fbf572>
2007-08-17 06:40:47 +0000

    Cleaned IndexFactory OBCache. Added missing comments etc.

--- src/main/java/org/ajmm/obsearch/index/IndexFactory.java
+++ src/main/java/org/ajmm/obsearch/index/IndexFactory.java
@@ -5,20 +5,27 @@ import org.ajmm.obsearch.Index;
 import com.thoughtworks.xstream.XStream;
 
 /**
- * An index factory creates Indexes from their serialized versions Currently we
+ * An index factory creates Indexes from their serialized versions. Currently we
  * use xstream http://xstream.codehaus.org/ to perform the
- * serialization/un-serialization process
+ * serialization/de-serialization process
  */
-public class IndexFactory {
+public final class IndexFactory {
+
+    /**
+     * Utility class should not have a public constructor.
+     */
+    private IndexFactory() {
+
+    }
 
     /**
      * Creates an index from the given XML. Users are responsible of knowing
      * what type of index is being read. Users are responsible of casting the
      * xml.
-     * @param xml
-     * @return
+     * @param xml String with the serialized index.
+     * @return An instantiated index.
      */
-    public static Index createFromXML(String xml) {
+    public static Index createFromXML(final String xml) {
         XStream xstream = new XStream();
         return (Index) xstream.fromXML(xml);
     }
