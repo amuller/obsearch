d58c3f13ea5cc61b66bfebaa780362874852212a
arnoldomuller <arnoldomuller@522650dd-5330-0410-bccd-6103c8fbf572>
2008-07-31 12:28:35 +0000

    template for  AbstractBucketContainerX

--- trunk/src/main/fm/net/obsearch/index/bucket/BucketContainer.java
+++ trunk/src/main/fm/net/obsearch/index/bucket/BucketContainer.java
@@ -2,10 +2,33 @@
 <#include "/@inc/ob.ftl">
 <#list types as t>
 <@type_info t=t/>
-<@binding_info t=t/>
 <@pp.changeOutputFile name="BucketContainer"+Type+".java" />
 package net.obsearch.index.bucket;
+/*
+		OBSearch: a distributed similarity search engine This project is to
+ similarity search what 'bit-torrent' is to downloads. 
+    Copyright (C) 2008 Arnoldo Jose Muller Molina
 
+  	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/** 
+	*  BucketContainer is a dummy class that is used to directly
+  *  inherit the functionality from AbstractBucketContainer${Type}.
+	*  
+  *  @author      Arnoldo Jose Muller Molina    
+  */
 import java.nio.ByteBuffer;
 
 import net.obsearch.Index;
