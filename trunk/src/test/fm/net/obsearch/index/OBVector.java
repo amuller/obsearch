58cee4ae85c1b5cac17ce71c0392c753961b664f
arnoldomuller <arnoldomuller@522650dd-5330-0410-bccd-6103c8fbf572>
2008-08-01 09:54:34 +0000

    generic testing for idistance

--- trunk/src/test/fm/net/obsearch/index/OBVector.java
+++ trunk/src/test/fm/net/obsearch/index/OBVector.java
@@ -1,38 +1,61 @@
+<@pp.dropOutputFile />
+<#include "/@inc/ob.ftl">
+<#list types as t>
+<@type_info t=t/>
+<@pp.changeOutputFile name="OBVector${Type}.java" />
 package net.obsearch.index;
+/*
+		OBSearch: a distributed similarity search engine This project is to
+    similarity search what 'bit-torrent' is to downloads. 
+    Copyright (C) 2008 Arnoldo Jose Muller Molina
 
+  	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
 import net.obsearch.exception.OBException;
-import net.obsearch.ob.OBShort;
+import net.obsearch.ob.OB${Type};
 /**
- * L1 distance implementation.
- * @author amuller
+ * L1 distance implementation for ${type}s.
+ * @author Arnoldo Jose Muller Molina
  *
  */
-public class OBVectorShort implements OBShort {
+<@gen_warning filename="OBVector.java "/>
+public class OBVector${Type} implements OB${Type} {
 	
-	private short[] data;
+	private ${type}[] data;
 	
 	/**
      * Default constructor must be provided by every object that implements the
      * interface OB.
      */
-    public OBVectorShort() {
+    public OBVector${Type}() {
     	data = null;
     }
 	
-	public OBVectorShort(short[] data){
+	public OBVector${Type}(${type}[] data){
 		this.data = data;
 	}
 
 	@Override
-	public short distance(OBShort object) throws OBException {
+	public ${type} distance(OB${Type} object) throws OBException {
 		// TODO Auto-generated method stub
-		OBVectorShort o = (OBVectorShort) object;
+		OBVector${Type} o = (OBVector${Type}) object;
 		assert data.length == o.data.length;
-		short res = 0;
+		${type} res = 0;
 		int i = 0;
 		while(i < data.length){
 			res += Math.abs(data[i] - o.data[i]);
@@ -41,23 +64,26 @@ public class OBVectorShort implements OBShort {
 		return res;
 	}
 
+	<@gen_warning filename="OBVector.java "/>
 	@Override
 	public void load(DataInputStream in) throws OBException, IOException {
 		int size = in.readInt();
-		data = new short[size];
+		data = new ${type}[size];
 		int i = 0;
 		while(i < size){
-			data[i] = in.readShort();
+			data[i] = in.read${Type}();
 			i++;
 		}
 	}
-
+	
+	<@gen_warning filename="OBVector.java "/>
 	@Override
 	public void store(DataOutputStream out) throws OBException, IOException {
 		out.writeInt(data.length);
-		for(short v : data){
-			out.writeShort(v);
+		for(${type} v : data){
+			out.write${Type}(v);
 		}
 	}
 
 }
+</#list>
